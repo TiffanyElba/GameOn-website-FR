9afb2a32ca40ff0319f0b457f609170da8b6cf64
Tiffany <tiffanyth.1812@gmail.com>
2024-10-04 21:24:15 +0200

    message d'erreur valide pour le champ prénom

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -12,6 +12,7 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const btnClose = document.querySelector(".close");
+const form = document.querySelector('form');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -27,3 +28,69 @@ btnClose.addEventListener("click", function(){
 });
 
 
+
+// fonction pendant la saisie d'un champ du formulaire 
+const baliseNom = document.getElementById('last');
+const balisePrenom = document.getElementById('first');
+const baliseEmail = document.getElementById('email');
+
+
+
+
+function verifierEmail(balise) {
+  let emailRegEx = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+");
+  if (emailRegEx.test(balise.value)) {
+    console.log("OK");
+  } else {
+    console.log("ERREUR");
+  }
+}
+
+function verifierChamp(balise) {
+  const errorMessageFirst = document.getElementById('errorMessageFirst');
+  if (balise.value === '' || balise.value.length < 2) {
+    errorMessageFirst.style.display = "block";
+  } else {
+    errorMessageFirst.style.display = "none";
+    console.log("OK");
+  }
+}
+
+// Ajout d'un écouteur d'événement sur le formulaire pour écouter le submit
+form.addEventListener("submit", (event) => {
+    // On empêche le comportement par défaut
+      event.preventDefault();
+});
+
+
+
+
+baliseNom.addEventListener('input', (event) => {
+    verifierChamp(baliseNom);
+});
+
+balisePrenom.addEventListener('input', (event) => {
+  verifierChamp(balisePrenom);
+});
+
+baliseEmail.addEventListener('input', (event) => {
+    verifierEmail(baliseEmail);
+});
+
+
+
+/*
+
+/*function verifierChamp(chaine) {
+  if (chaine.length < 2) {
+    throw new Error(`La chaîne ${chaine} est trop courte`)
+  }
+}
+
+try {
+  verifierChamp(baliseNom.value);
+
+} catch(erreur) {
+  console.log(erreur);
+}
+*/
